1cb4ab13949d7c33cae76c8fa54fb0c3f9dfca81
N.Sanjeev kumar <sanjeevitcit@gmail.com>
2020-12-17 21:06:54 +0530

    Recursion: all combination for balanced parenthesis
    Signed-off-by: N.Sanjeev kumar <sanjeevitcit@gmail.com>

---
+++ Recursion/bas_balanced_paren.cpp
@@ -0,0 +1,41 @@
+/*
+ * https://www.geeksforgeeks.org/print-all-combinations-of-balanced-parentheses/
+ *
+ * {{{}}} 111222
+ * {{}{}} 112122
+ * {{}}{} 112212
+ * {}{{}} 121122
+ * {}{}{} 121212
+ */
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char a[100];
+
+void gen_possiblities(int n, int open, int close, int pos){
+    if (close == n){
+        cout<<a<<"\n";
+        return;
+    }
+    if (open < n){
+        a[pos]= '1';
+        gen_possiblities(n, open+1, close, pos+1);
+    }
+    if (close < open){
+        a[pos]= '2';
+        gen_possiblities(n, open, close+1, pos+1);
+    }
+
+}
+
+int main()
+{
+    int n=3;
+    char a[2*n]={NULL};
+    gen_possiblities(n, 0, 0, 0);
+    return 0;
+}
